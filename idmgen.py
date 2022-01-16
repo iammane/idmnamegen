216c98de458dc22c5ff3875e937da58396afce7c
iammane <92211151+iammane@users.noreply.github.com>
2022-01-16 14:22:48 -0500

    Update idmgen.py
    Refactored into a function, it's still a bit sloppy but compared to original it's definitely cleaner. Unsure if I've reduced the overall amount of lines...

--- idmgen.py
+++ idmgen.py
@@ -1,43 +1,45 @@
-'''
-IDM name generator v0.0001
-Conceived from review of https://github.com/wjt/flitwemmmmm - was trying to see
-how this worked and then said 'Welp, I can just do wacky string exercises in
-Python.'
+# IDM name generator v1 - 1/16/2022
+# Conceived from review of https://github.com/wjt/flitwemmmmm - was trying to see
+# how this worked and then said 'Welp, I can just do wacky string exercises in
+# Python.'
 
-At some point would like to maybe jam this into a web front end. Should
-also probably make this a function proper :)
-'''
+# At some point would like to maybe jam this into a web front end.
+# Changelog:
+# 1/16/2022 - Refactored into a function rather than a straight through
+# script. Could still be cleaner but maybe another day.
+
 
 import random
-t = ''
-fh = open('data').readlines()
 
-while t != 'q':
-    # The line below is how many times the data list is iterated thru
+def runTheIDM(t, newarr, counter):
+    c = 0
     iterlist = [1, 2, 3]
-    c = 0  # Reset counter
-
-    t = input('Please enter a word - q to quit: ')
-
-    if t != 'q':
-        counter = int(input('Please enter iteration int: ') or 5)
-
-    newarr, titlearr = [], []
-    titlearr.append(t)  # Add the entered word to start of list
-    for each in fh:
-        # Fill newarr with all track titles in data file
-        newarr.append(each.strip())
-
-    while c < counter and t != 'q':
-        # Loop either 1, 2 or 3 times (pending iteration int)
-        for iters in range(0, random.choice(iterlist)):
+    titlearr = []
+    titlearr.append(t)
+    while c < counter:
+        # Loop either 1, 2 or 3 times - this keeps resulting phrase short
+        for _ in range(0, random.choice(iterlist)):
             # Select a random song title from the list
             randtit = random.choice(newarr)
             # Append the song title to new title, index 1
-            # through 10, skipping every other char
-            titlearr.append(randtit[1:100:2])  # Was sliced as 1:10:2
+            # through 100, skipping every other char
+            titlearr.append(randtit[1:100:2])
         titlefin = ''.join(titlearr)  # Munge it all together
         print(titlefin)  # Print it
         titlearr = [t]  # Reset to originally entered word
         titlefin = ''  # Clear the title
         c += 1  # Increase the counter
+
+if __name__ == '__main__':
+    t = ''
+    fh = open('data').readlines()
+    newarr = []
+    for each in fh:
+        newarr.append(each.strip())
+    while t.lower() != 'q':
+        t = input('Please enter a word - q to quit: ')
+        if t.lower() == 'q':
+            break
+        else:
+            counter = int(input('Please enter an iteration integer: ') or 5)
+        runTheIDM(t, newarr, counter)
